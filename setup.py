fb1818b8340e2041486326c4ff99c263afbc9ce9
Roland Puntaier <roland.puntaier@gmail.com>
2020-04-06 16:48:56 +0200

    v0.2.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def main():
         readme = readme_file.read()
     setuptools.setup(
         name="chcko-r",
-        version = "0.2.1",
+        version = "0.2.2",
         description="A random mix of exercises for chcko",
         long_description=readme,
         long_description_content_type="text/x-rst",
